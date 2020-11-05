63df4bd903e46c633cdf409e09459c4206c1ae9e
Felix de Pablo <f.depablo.lobo@gmail.com>
2020-11-05 17:02:01 +0100

    arreglada espadda

--- 00_Java/src/videojuego/Espada.java
+++ 00_Java/src/videojuego/Espada.java
@@ -1,27 +1,11 @@
 package videojuego;
 
 public class Espada extends Arma{
-	private int daño;
-	private String tipo;
 	
 	@Override
 	public void usar() {
 		System.out.println("Swannnnnnggg");
-		System.out.println("Tipo: " + tipo + " daño: " + daño);
+		System.out.println("Tipo: " + getTipo() + " daño: " + getDaño());
 	}
-	
-	public int getDaño() {
-		return daño;
-	}
-	public void setDaño(int daño) {
-		this.daño = daño;
-	}
-	public String getTipo() {
-		return tipo;
-	}
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-	
 	
 }
